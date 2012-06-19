91918ebaaee61c561334740bd54c70dda26ac331
nahong01 <nahong01@gmail.com>
2012-06-19 10:32:51 +0900

    20120619 chapter 6-4-2 ~

---
+++ study01/20120619/node.chapter6-4-2.js
@@ -0,0 +1,14 @@
+var http    = require('http');
+
+http.createServer(function (request, response) {
+    if (request.method == 'GET') {
+        console.log('GET 요청입니다.');
+    } else if (request.method == 'POST') {
+        console.log('POST 요청입니다.');
+    }
+}).listen(4444
+         ,function () {
+              console.log('Server Running at http://127.0.0.1:4444');
+          }
+         );
+;
