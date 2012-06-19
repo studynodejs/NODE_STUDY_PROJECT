3af432c9cbdddd9ef87c28dc1188a22b00eb09c2
nahong01 <nahong01@gmail.com>
2012-06-19 16:38:25 +0900

    node.chapter3-4-2.js 수정소스 ( listen 분리, request 이벤트 연결 핸들링)

--- study01/20120619/node.chapter6-4-2.js
+++ study01/20120619/node.chapter6-4-2.js
@@ -1,13 +1,23 @@
+// http 모듈
 var http = require('http');
 
-http.createServer(function (request, response) {
-    if (request.method == 'GET') {
-        console.log('GET 요청입니다.');
-    } else if (request.method == 'POST') {
-        console.log('POST 요청입니다.');
-    }
-}).listen(4444
-         ,function () {
-              console.log('Server Running at http://127.0.0.1:4444');
-          }
-         );
+// 서버생성
+var server = http.createServer();
+
+// 서버구동
+server.listen(4444, function() {
+  console.log('Server Running at http://127.0.0.1:4444');
+});
+
+// request 요청시
+server.on('request', function (request, response) {
+  if (request.method == 'GET') {
+    console.log('GET 요청입니다.');
+  } else if (request.method == 'POST') {
+    console.log('POST 요청입니다.');
+  }
+
+  // client response
+  response.writeHead(200,{'Content-Type': 'text/html;charset=utf-8'});
+  response.end('<H>요청하셨습니까~~~~?</H>');
+});
