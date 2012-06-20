afcd6da88bcfa56c5a3741270407416568b21f3f
nahong01 <nahong01@gmail.com>
2012-06-20 15:40:24 +0900

    chapter6-4-4 added

--- study01/20120619/node.chapter6-4-4.js
+++ study01/20120619/node.chapter6-4-4.js
@@ -1,9 +1,6 @@
 // 모듈
 var http  = require('http');
-<<<<<<< HEAD
-=======
 var url   = require('url');
->>>>>>> 25d2600bad27a3cc3ce10077ce10bfe093440cf0
 var fs    = require('fs');
 
 
@@ -27,28 +24,10 @@ server.listen(4444, function() {
 
 // request 요청시
 server.on('request', function (request, response) {
-<<<<<<< HEAD
   var req_gb  = '';
-=======
-<<<<<<< .merge_file_a07480
-  var req_gb  = '';
-=======
->>>>>>> 25d2600bad27a3cc3ce10077ce10bfe093440cf0
-  var query = 'post';
->>>>>>> .merge_file_a05188
   if (request.method == 'GET') {
     console.log('GET 요청입니다.');
-<<<<<<< HEAD
-
     contents  = contents1;
-
-=======
-<<<<<<< .merge_file_a07480
-    contents  = contents1;
-=======
-    console.log(query);
->>>>>>> .merge_file_a05188
->>>>>>> 25d2600bad27a3cc3ce10077ce10bfe093440cf0
   } else if (request.method == 'POST') {
     request.on('data'
               ,function (data){
@@ -58,7 +37,7 @@ server.on('request', function (request, response) {
     contents  = contents2;
     console.log('POST 요청입니다.');
   }
-
+  // client response
   response.writeHead(200,{'Content-Type': 'text/html;charset=utf-8'});
   response.end(contents);
 });
