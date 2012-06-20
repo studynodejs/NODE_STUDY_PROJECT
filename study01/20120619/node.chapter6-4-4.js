f577fd2637c0c31b1a8ccfb54c7bac832394a1cc
nahong01 <nahong01@gmail.com>
2012-06-20 15:25:28 +0900

    chapter6.4.4 source added
    progit.ko.pdf added

--- study01/20120619/node.chapter6-4-4.js
+++ study01/20120619/node.chapter6-4-4.js
@@ -1,6 +1,17 @@
 // 모듈
 var http  = require('http');
-var url   = require('url');
+var fs    = require('fs');
+
+
+/* html 불러오기 */
+var contents  = '';
+var contents1 = '';
+var contents2 = '';
+fs.readFile('c:/PROJECT/workspace2/NODE_STUDY_PROJECT/study01/20120619/node.chapter6-4-4.html'
+           ,function (error, data) {
+              contents1 = data;
+            }
+           );
 
 // 서버생성
 var server = http.createServer();
@@ -12,20 +23,23 @@ server.listen(4444, function() {
 
 // request 요청시
 server.on('request', function (request, response) {
+  var req_gb  = '';
   var query = 'post';
   if (request.method == 'GET') {
-    query = url.parse(request.url, true).query;
     console.log('GET 요청입니다.');
-    console.log(query);
+
+    contents  = contents1;
+
   } else if (request.method == 'POST') {
+    request.on('data'
+              ,function (data){
+                 contents2 = '<h1>'+data+'</h1>';
+               }
+              );
+    contents  = contents2;
     console.log('POST 요청입니다.');
-    console.log(query);
   }
 
-  var contents  = '<H>요청하셨습니까~~~~?</H>';
-      contents += '<br>';
-      contents += '<H>' + JSON.stringify(query) + '</H>';
-  // client response
   response.writeHead(200,{'Content-Type': 'text/html;charset=utf-8'});
   response.end(contents);
 });
