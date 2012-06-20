c52a41844cf34d17997279dbb1b8ee85407aec5c
nahong01 <nahong01@gmail.com>
2012-06-20 14:45:58 +0900

    chapter6.4.4 source added progit.ko.pdf added

---
+++ study01/20120619/node.chapter6-4-4.js
@@ -0,0 +1,43 @@
+// 모듈
+var http  = require('http');
+var url   = require('url');
+var fs    = require('fs');
+
+
+/* html 불러오기 */
+var contents  = '';
+var contents1 = '';
+var contents2 = '';
+fs.readFile('c:/PROJECT/workspace2/NODE_STUDY_PROJECT/study01/20120619/node.chapter6-4-4.html'
+           ,function (error, data) {
+              contents1 = data;
+            }
+           );
+
+// 서버생성
+var server = http.createServer();
+
+// 서버구동
+server.listen(4444, function() {
+  console.log('Server Running at http://127.0.0.1:4444');
+});
+
+// request 요청시
+server.on('request', function (request, response) {
+  var req_gb  = '';
+  if (request.method == 'GET') {
+    console.log('GET 요청입니다.');
+    contents  = contents1;
+  } else if (request.method == 'POST') {
+    request.on('data'
+              ,function (data){
+                 contents2 = '<h1>'+data+'</h1>';
+               }
+              );
+    contents  = contents2;
+    console.log('POST 요청입니다.');
+  }
+  // client response
+  response.writeHead(200,{'Content-Type': 'text/html;charset=utf-8'});
+  response.end(contents);
+});
