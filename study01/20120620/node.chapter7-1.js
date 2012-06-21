7c702d726d9e1adfaff6ebce867008471a799a78
nahong01 <nahong01@gmail.com>
2012-06-21 10:21:46 +0900

    chapter7-1
    added

---
+++ study01/20120620/node.chapter7-1.js
@@ -0,0 +1,29 @@
+// 모듈
+var http  = require('http');
+var fs    = require('fs');
+var ejs   = require('ejs');
+var contents  = '';
+
+// ejs file read
+fs.readFile('c:/PROJECT/workspace2/NODE_STUDY_PROJECT/study01/20120620/node.chapter7-1.ejs'
+           ,'utf-8'
+           ,function (error, data) {
+              contents = data;
+              console.log(contents);
+            }
+           );
+
+// 서버생성
+var server = http.createServer();
+
+// 서버구동
+server.listen(4444, function() {
+  console.log('Server Running at http://127.0.0.1:4444');
+});
+
+// request 요청시
+server.on('request', function (request, response) {
+  // client response
+  response.writeHead(200,{'Content-Type': 'text/html;charset=utf-8'});
+  response.end(ejs.render(contents));
+});
