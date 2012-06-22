f2618b6ceea79cba9f825f00b90e494ba26d7ff5
nahong01 <nahong01@gmail.com>
2012-06-22 17:16:54 +0900

    chapter7-3 added

--- study01/20120620/node.chapter7-3-1.js
+++ study01/20120620/node.chapter7-3-1.js
@@ -1,33 +1,17 @@
 // 모듈
-var http  = require('http');
-var fs    = require('fs');
-var jade  = require('c:/nodejs/node_modules/jade');
-var contents;
-
-// ejs file read
-fs.readFile('c:/PROJECT/workspace2/NODE_STUDY_PROJECT/study01/20120620/node.chapter7-2-3.jade'
-           ,'utf-8'
-           ,function (error, data) {
-              contents = jade.compile(data);
-              console.log(data);
-              //console.log(contents());
-            }
-           );
+var connect  = require('c:/nodejs/node_modules/connect');
 
 // 서버생성
-var server = http.createServer();
-
-// 서버구동
-server.listen(4444, function() {
-  console.log('Server Running at http://127.0.0.1:4444');
-});
-
-// request 요청시
-server.on('request', function (request, response) {
+var server = connect.createServer(function (request, response, next) {
+  console.log('첫번째 함수 호출');
+  next();
+},function (request, response, next) {
+  console.log('두번째 함수 호출');
+  next();
+},function (request, response, next) {
   // client response
   response.writeHead(200,{'Content-Type': 'text/html;charset=utf-8'});
-  response.end(contents({
-    name: 'nahong'
-   ,discription: 'test value ???'
-  }));
+  response.end('<h1> CONNECT MODULE </h1>');
+}).listen(4444, function() {
+  console.log('Server Running at http://127.0.0.1:4444');
 });
--- study01/20120620/node.chapter7-3.js
+++ study01/20120620/node.chapter7-3.js
@@ -1,35 +1,12 @@
 // 모듈
-var http  = require('http');
+var connect  = require('c:/nodejs/node_modules/connect');
 
 // 서버생성
-var server = http.createServer();
-
-// 서버구동
-server.listen(4444, function() {
-  console.log('Server Running at http://127.0.0.1:4444');
-});
-
-// request 요청시
-server.on('request', function (request, response) {
-  var cookie  = request.headers.cookie;
-  console.log(cookie);
-
-  if (cookie) {
-    cookie  = cookie.split(';').map(function (element){
-                var element = element.split('=');
-                return {
-                        key   : element[0]
-                       ,value : element[1]
-                       };
-              });
-  } else {
-    cookie = "none";
-  }
-
+var server = connect.createServer(function (request, response) {
   // client response
-  response.writeHead(200,{'Content-Type': 'text/html;charset=utf-8'
-                         ,'Set-Cookie'  : ['name=nahong','region=Seoul']
-                         }
-                    );
-  response.end('<h1>' + JSON.stringify(cookie) + '</h1>');
+  console.log('test');
+  response.writeHead(200,{'Content-Type': 'text/html;charset=utf-8'});
+  response.end('<h1> CONNECT MODULE </h1>');
+}).listen(4444, function() {
+  console.log('Server Running at http://127.0.0.1:4444');
 });
