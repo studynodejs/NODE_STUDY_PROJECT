dfb03347059e4f7d179bf9e5bbaf658b3577e590
nahong01 <nahong01@gmail.com>
2012-06-25 15:07:43 +0900

    chapter 7-4-3
    에러핸들러 옵션들에 true&false 의 차이점을 모르겠다. 결과가 다 똑같다.

--- study01/20120620/node.chapter7-4-3.js
+++ study01/20120620/node.chapter7-4-3.js
@@ -2,13 +2,14 @@
 var connect  = require('c:/nodejs/node_modules/connect');
 
 // 서버생성
-connect.createServer(connect.logger('short')
-                    ,connect.query()
-                    ,function (request, response) {
-  throw new Error('Page Not Found');
-  // client response
-  response.writeHead(200,{'Content-Type': 'text/html;charset=utf-8'});
-  response.end('<h1>' + JSON.stringify(request.query) + '</h1>');
-}).listen(4444, function() {
+connect.createServer(function (request, response) {
+                       throw new Error('Page Not Found');
+                     }
+                    ,connect.errorHandler({
+                                           stack:false
+                                          ,message:false
+                                          ,dump:false
+                     })
+).listen(4444, function() {
   console.log('Server Running at http://127.0.0.1:4444');
 });
