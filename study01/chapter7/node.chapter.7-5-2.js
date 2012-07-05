16fd0e6d730137e21e3192f1e0406ce28c9c1de9
nahong01 <nahong01@gmail.com>
2012-07-05 12:57:45 +0900

    chapter 7-5 connect.router >> url.parse

---
+++ study01/chapter7/node.chapter.7-5-2.js
@@ -0,0 +1,25 @@
+// 모듈
+var connect  = require('c:/nodejs/node_modules/connect');
+var url      = require('url');
+
+// 서버생성
+var server = connect.createServer(connect.logger(),function (request, response) {
+  // 라우터 사용하는 대신에 6장에서 나온 url을 이용해서 변경
+  var pathname = url.parse(request.url).pathname;
+  if (pathname=="/Home/Index" ) {
+    response.writeHead(200, {'Content-Type':'text/html'});
+    response.write('<h1>Index Page</h1>');
+    response.end();
+  }else if (pathname=="/Home/About" ) {
+    response.writeHead(200, {'Content-Type':'text/html'});
+    response.write('<h1>About Page</h1>');
+    response.end();
+  }else{
+    response.writeHead(200, {'Content-Type':'text/html'});
+    response.write('<h1>///////////////</h1>');
+    response.end();
+  }
+}).listen(4444, function() {
+  console.log('Server Running at http://127.0.0.1:4444');
+});
+
