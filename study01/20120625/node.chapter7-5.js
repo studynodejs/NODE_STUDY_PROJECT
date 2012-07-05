9e834b3501495ffa094d6806851902fa8d3a21ae
nahong01 <nahong01@gmail.com>
2012-07-05 09:11:37 +0900

    request , response add

--- study01/20120625/node.chapter7-5.js
+++ study01/20120625/node.chapter7-5.js
@@ -4,14 +4,14 @@ var connect  = require('c:/nodejs/node_modules/connect');
 // 따라서 app.get 함수도 찾지 못한다.
 // 그럼 현재 코딩되어있는 방식이 아닌
 // 다른 방식으로 변경해서 수정해봐야겠다.
-connect.createServer(connect.router(function (app) {
-  app.get('Home/Index', function () {
+connect.createServer(connect.middleware.router(function (app) {
+  app.get('Home/Index', function (request, response) {
     response.writeHead(200, {'Content-Type':'text/html'});
     response.write('<h1>Index Page</h1>');
     response.end();
   });
 
-  app.get('Home/About', function () {
+  app.get('Home/About', function (request, response) {
     response.writeHead(200, {'Content-Type':'text/html'});
     response.write('<h1>About Page</h1>');
     response.end();
