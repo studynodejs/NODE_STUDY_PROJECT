d7eca911c8a7490a42f58c47c7b0bdc1df33a771
nahongubuntu <nahong01@gmail.com>
2012-07-17 16:28:01 +0900

    nahonglinux : 2012.07.17 > Chapter 7-8 connect module - session middleware 사용               * session middleware 사용시 주의 사항 주석 확인할것

---
+++ study01/chapter7/node.chapter.7-8.js
@@ -0,0 +1,33 @@
+// module require
+var connect         = require('/usr/lib/node_modules/connect');         // connect
+var connect_router  = require('/usr/lib/node_modules/connect_router');  // connect_router middleware
+
+// create server
+var server  = connect.createServer();
+
+// use middleware
+server.use(connect.logger());
+server.use(connect.cookieParser());
+//server.use(connect.session());
+// session middleware 사용시 secret를 반드시 선언해주어야 하는데...... 사용법은 현재 정확히 파악이 안됨....
+server.use(connect.session({secret:'test'}));
+server.use(function (request, response){
+  // variable
+  var output = '';
+  output    += '<p>Cookies: '   + JSON.stringify(request.cokies)  + '</p>';
+  output    += '<h1>Sessioin: ' + JSON.stringify(request.session) + '</h1>';
+
+  //response
+  response.writeHead(200,{'Content-Type': 'text/html'});
+  response.end(output);
+
+  // save session
+  //response.session.now  = (new Date()).toUTCString();
+  // 세션을 저장시 response 에 하면 안되고 request 쪽에다가 해줘야 한다.
+  request.session.now  = (new Date()).toUTCString();
+});
+
+// run server
+server.listen(4444, function () {
+  console.log('server running at http://192.168.56.101:4444');
+});
