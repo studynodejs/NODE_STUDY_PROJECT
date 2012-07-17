3c79806a0e3be0665d9133747a1a9502a201b09e
nahongubuntu <nahong01@gmail.com>
2012-07-17 15:23:40 +0900

    nahonglinux : 2012.07.17 > chapter7-7 error code clear               * 파일 호출시 절대경로를 상대경로로 수정               * 쿠키값 비교시 에러 수정

--- study01/chapter7/node.chapter.7-7.login.js
+++ study01/chapter7/node.chapter.7-7.login.js
@@ -18,17 +18,17 @@ server.use(connect_router(function (app){
   // GET : /Login
   app.get('/Login'
          ,function (request, response){
-            if (request.cookies.auth == true){
+            // 쿠키에 저장되어 있는 값은 문자열이다 문자열 비교를 해주어야 한다.(책에서는 boolean값으로 비교해서 오류)
+            if (request.cookies.auth == 'true'){
               // 로그인되어 있는 경우는
               response.writeHead(200,{'Content-Type': 'text/html'});
               response.write('<h1>Login Success</h1>');
               response.end();
             }else{
               // 로그인 되어 있지 않은 경우
-              fs.readFile('/node.chapter.7-7.login.html', function (error, data){
+              fs.readFile('./node.chapter.7-7.login.html', function (error, data){
                 response.writeHead(200,{'Content-Type': 'text/html'});
-                response.write(data);
-                response.end();
+                response.end(data);
               });
             }
           }
@@ -47,6 +47,7 @@ server.use(connect_router(function (app){
       // 로그인 실패시
       response.writeHead(200,{'Content-Type': 'text/html'});
       response.write('<h1>Login FAIL!!!</h1>');
+      response.end();
     }
   });
 }));
