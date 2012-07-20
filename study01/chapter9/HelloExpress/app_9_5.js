c7827d2c87ea176820cf94bdb5f88a7470e2d215
nahongubuntu <nahong01@gmail.com>
2012-07-20 16:39:13 +0900

    nahonglinux : 2012.07.20 > Chapter 9-5 layout page               * 안먹는다....ㅡㅡa               * 왜 안먹는지 나중에 고민해보자....ㅡㅡa

---
+++ study01/chapter9/HelloExpress/app_9_5.js
@@ -0,0 +1,66 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes  = require('./routes')
+  , http    = require('http');
+
+var app = express();
+
+app.configure(function(){
+  app.set('case sensitive routes', true);
+  app.set('port', process.env.PORT || 4444);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+/* ############################################################################
+ * nahong conding start
+ * ############################################################################
+ * */
+// chapter 9-5
+
+// GET : /product
+app.get('/product',function(request, response){
+  response.render('product',{
+    layout: false,
+    title: 'Product Page'
+  });
+});
+
+// GET : /product/insert
+app.get('/product/insert',function(request, response){
+  response.render('product/insert',{
+    layout: 'layout_9_5.jade',
+    title: 'Insert Page'
+  });
+});
+
+// GET : /product/edit
+app.get('/product/edit',function(request, response){
+  response.render('product/edit',{
+    layout: 'layout_9_5.jade',
+    title: 'Edit Page'
+  });
+});
+
